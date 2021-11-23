e6bff9e1d06450a9f57a44c154d5f40b50ef826e
darse73 <k.yu.713558.2016113@gmail.com>
2021-11-24 04:45:24 +0900

    headerを改良しました。

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,37 @@
 <template>
   <div class="header">
-    <h1>ヘッダー</h1>
+    <h1 class="title">ヘッダー</h1>
+    <div class="serch">
+      <input v-on:click="frequentlySerched" type="text" class="serch-text">
+      <div class="serch-mark">🔍</div>
+    </div>
+    <img src="../assets/menu.png" alt="メニュー" class="menu-mark">
   </div>
 </template>
 
 <style scoped>
 .header {
   background-color: red;
+  height: 5rem;
+  display: flex;
+  justify-content: space-around;
+}
+.title {
+  line-height: 5rem;
+}
+.serch {
+  display: flex;
+}
+.serch-text {
+  margin: 1.5rem;
+}
+.serch-mark {
+  line-height: 5rem;
+  font-size: 2rem;
+}
+.menu-mark {
+  width: 2rem;
+  height: 2rem;
+  margin: 1.5rem;
 }
 </style>
