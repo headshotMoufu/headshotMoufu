097ccb97a98ecf06c59d937a3d0ab008f36c9d34
志水佑輔 <shimizuyuusuke@shimizuyuusukenoMacBook-Pro.local>
2021-12-08 13:54:35 +0900

    “Youtubedouga”

--- src/views/ToYoutubeList.vue
+++ src/views/ToYoutubeList.vue
@@ -92,6 +92,8 @@ export default {
       results2: null,
       tmp_results3: [], //掘り出し物の動画を格納する配列
       results3: null, //埋もれてる動画結果
+
+      count: 0,
       keyword: "J-POP",
       params: {
         q: "", // 検索クエリを指定します。
@@ -104,8 +106,9 @@ export default {
           video
         */
         maxResults: "1", // 最大検索数（0以上50以下）
-        order: "viewCount", // リソースを再生回数の多い順に並べます。
-        //publishedBefore: null,
+        order: "date", // リソースを再生回数の多い順に並べます。
+        publishedBefore: null,
+        //publishedAfter: null,
         /*
           date – リソースを作成日の新しい順に並べます。
           rating – リソースを評価の高い順に並べます。
@@ -116,7 +119,7 @@ export default {
         */
         //publishedAfter:,  publishedAfter パラメータは、指定した日時より後に作成されたリソースのみが API レスポンスに含まれるように指定します。この値は RFC 3339 形式の date-time 値です（1970-01-01T00:00:00Z）。
         //publishedBefore:,  publishedBefore パラメータは、指定した日時より前に作成されたリソースのみが API レスポンスに含まれるように指定します。
-        key: "AIzaSyBiISEotpsIDifCOskeHUpfopKU1Zmq8Lw",
+        key: "AIzaSyA2RzZ-SEU9GCN1wbNSAWg_F7VXiBFBgG0",
       },
       params2: {
         //動画情報所得のためのパラメータ
@@ -126,15 +129,16 @@ export default {
       },
     }
   },
+  props: {
+    msg: String,
+  },
   methods: {
     search_video: function () {
-      for (let i = 0; i < 3; i++) {
-        setTimeout(() => {
-          this.search_rule()
-        }, 10000)
-      }
-    },
-    search_rule: function () {
+      this.results3 = null
+      console.log(this.results3)
+      let day = new Date()
+      day.setDate(day.getDate() - 30 - this.count)
+      this.params.publishedBefore = day
       this.params.q = this.keyword
       var self = this
       axios
@@ -142,18 +146,23 @@ export default {
           params: this.params,
         })
         .then(function (res) {
-          self.tmp_results.push(res.data.items[0])
-          console.log(self.tmp_results[0])
+          self.tmp_results = res.data.items
+          console.log(self.tmp_results)
           self.tmp_results3.push({
             title: self.tmp_results[0].snippet.title,
             video_id: self.tmp_results[0].id.videoId,
             url: self.tmp_results[0].snippet.thumbnails.medium.url,
           })
-          console.log("test")
           self.results3 = self.tmp_results3
-          console.log(self.results3)
+          self.count += 1
+          if (self.count < 2) {
+            setTimeout(self.search_video, 2000)
+          } else {
+            self.count = 0
+          }
         })
     },
+    search_rule: function () {},
   },
 }
 </script>
